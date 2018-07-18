7225d9ac69ef153d2d9640c4d34f7413864f59af
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-18 18:43:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,15 @@ end
 
 
 
-def long_planeteer_calls()
-  
+def long_planeteer_calls(array)
+  if array.length.any? > 4
+    true
+  else
+    false
+  end
 end
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
