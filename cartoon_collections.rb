eb3e51b9a6f5542889ab4760dff439c4f7a69647
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-18 18:27:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,12 @@
-def roll_call_dwarves(dwarves) # code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |rollcall, index|
     puts "#{index+1}. #{rollcall}"
   end
 end
 
-def summon_captain_planet() # code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  
+  
 end
 
 def long_planeteer_calls# code an argument here
