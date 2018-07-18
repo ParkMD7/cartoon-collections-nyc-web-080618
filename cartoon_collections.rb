e8388f195cf0a6af4a8360b023b22942405b66f6
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-18 18:35:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = []
   planeteer_calls.collect do |capitalize_exclaim|
-    puts { {capitalize_exclaim.capitalize} + "!" }
+    puts { capitalize_exclaim.capitalize + "!" }
   end
 end
 
